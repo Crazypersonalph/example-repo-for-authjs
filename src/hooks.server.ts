9a876160b2883b3a9462350ea7875d99f9f6ed64
Crazypersonalph <93847055+Crazypersonalph@users.noreply.github.com>
2023-04-08 10:34:40 +0800

    Initial commit

---
+++ src/hooks.server.ts
@@ -0,0 +1,35 @@
+import { SvelteKitAuth } from '@auth/sveltekit'
+import AzureADProvider from '@auth/core/providers/azure-ad'
+import type { Provider } from '@auth/core/providers';
+import { redirect, type Handle } from '@sveltejs/kit';
+import { sequence } from '@sveltejs/kit/hooks';
+
+async function authorization({ event, resolve }) {
+    // Protect any routes under /authenticated
+    if (event.url.pathname.startsWith('/')) {
+   const session = await event.locals.getSession();
+        if (!session) {
+            throw redirect(303, '/auth/signin');
+        }
+        console.log(session.user.image)
+    }
+
+    // If the request is still here, just proceed as normally
+    return resolve(event);
+}
+
+
+export const handle: Handle = sequence( 
+    SvelteKitAuth({
+    providers: [
+      AzureADProvider({ 
+        clientId: '', // ClientID, 
+        clientSecret: '', // Client Secret
+        tenantId: '', // TenantID
+        }),
+    ] as Provider[],
+    secret: '', // Auth Secret
+    trustHost: true
+    }),
+    authorization
+)
---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { signIn, signOut } from '@auth/sveltekit/client';
+	import { page } from '$app/stores';
+</script>
+
+<p>
+	{#if Object.keys($page.data.session || {}).length}
+		<strong>{$page.data.session?.user?.image}</strong>
+		<span class="signedInText">
+			<small>Signed in as</small><br />
+			<strong>{$page.data.session?.user?.email || $page.data.session?.user?.name}</strong>
+		</span>
+		<button on:click={() => signOut()} class="button">Sign out</button>
+	{/if}
+</p>
